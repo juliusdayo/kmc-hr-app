bb268e0017d8021e6f36a4254c6102741ec55481
juliusdayo <72971299+juliusdayo@users.noreply.github.com>
2024-10-24 13:56:52 +0800

    Shadcn button install

--- src/app/hr/page.js
+++ src/app/hr/page.js
@@ -1,6 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 const Index = () => {
     return (
         <div>
+            <Button>Click me</Button>
             <h1>HR</h1>
         </div>
     );
