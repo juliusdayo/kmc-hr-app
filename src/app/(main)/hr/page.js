960fad5a49b1e9815cbe0e430bed854dbe082ad1
juliusdayo <72971299+juliusdayo@users.noreply.github.com>
2024-10-24 22:44:11 +0800

    Add more routes

--- src/app/(main)/hr/page.js
+++ src/app/(main)/hr/page.js
@@ -6,7 +6,7 @@ import { ApplicantPool } from "@/components/main/applicants/applicantPool";
 import { JobPosting } from "@/components/main/jobs/jobPosting";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { hiringManager } from "@/constants";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Index = () => {
     const [nav, setNav] = useState("JobListings")
@@ -14,17 +14,27 @@ const Index = () => {
     const handleNav = (screen) => {
         setNav(screen)
     }
+
+    const fetchApplicant = async () => {
+        const response = await fetch('/api/applicant')
+        const data = await response.json()
+        console.log(data)
+    }
+    useEffect(() => {
+        fetchApplicant()
+    }
+        , [])
     return (
         <SidebarProvider className="w-full">
-            <AppSidebar screens={hiringManager} setNav={handleNav}/>
+            <AppSidebar screens={hiringManager} setNav={handleNav} />
             <main className="w-full">
                 <div className="p-8 w-full space-y-5">
                     <Header>
                         <SidebarTrigger />
                     </Header>
                     <div>
-                        {nav === "JobListings" ? <JobPosting/> :
-                         nav === "ApplicantRatings" ? <ApplicantPool/>: null }
+                        {nav === "JobListings" ? <JobPosting /> :
+                            nav === "ApplicantRatings" ? <ApplicantPool /> : null}
                     </div>
                 </div>
             </main>
--- src/app/api/applicant/route.js
+++ src/app/api/applicant/route.js
@@ -4,10 +4,14 @@ import { initializeDb } from '../util/db';
 
 
 export async function GET() {
-    const db = initializeDb()
-    const applicantCollection = collection(db, 'test')
-    const applicantSnapshot = await getDocs(applicantCollection)
-    const applicantList = applicantSnapshot.docs.map(doc => doc.data())
+    try {
+        const db = initializeDb()
+        const applicantCollection = collection(db, 'applicant')
+        const applicantSnapshot = await getDocs(applicantCollection)
+        const applicantList = applicantSnapshot.docs.map(doc => doc.data())
 
-    return Response.json({ data: applicantList })
+        return Response.json({ data: applicantList })
+    } catch (e) {
+        return Response.json({ data: 'error' })
+    }
 }
