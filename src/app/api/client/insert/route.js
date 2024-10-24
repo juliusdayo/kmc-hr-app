faf66a441c723837b6406c8b6ba88475f41267a0
juliusdayo <72971299+juliusdayo@users.noreply.github.com>
2024-10-24 20:42:11 +0800

    Make the dbname a var

--- src/app/api/client/insert/route.js
+++ src/app/api/client/insert/route.js
@@ -3,14 +3,14 @@ import { initializeDb } from "../../util/db";
 import { v4 as uuidv4 } from 'uuid';
 
 export async function POST(req, res) {
-
+    const dbName = 'client'
     const db = initializeDb();
     const data = await req.json();
 
 
     for (let i of data) {
         console.log(i);
-        setDoc(doc(db, "test", uuidv4()), i)
+        setDoc(doc(db, dbName, uuidv4()), i)
 
     }
 
