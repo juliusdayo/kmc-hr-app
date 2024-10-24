525401a1f1844025073d69285d36e4f616512014
Jam <103874277+robin-dc@users.noreply.github.com>
2024-10-24 18:40:36 +0800

    added page application status page

---
+++ src/app/(main)/applicant/(tabs)/applications/page.js
@@ -0,0 +1,47 @@
+"use client"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    Dialog,
+    DialogTrigger,
+ } from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button"
+import { ApplicationDetails } from "../../components/applicationDetails";
+
+const Applications = () => {
+    return (
+        <Dialog>
+            <Card className="overflow-hidden">
+                <CardHeader className="bg-white">
+                    <CardTitle className="pt-3">Application Statuses</CardTitle>
+                    <CardDescription>Full Stack Developer</CardDescription>
+                </CardHeader>
+                <CardContent className="bg-white p-6 space-y-3">
+                    <div className="space-y-4">
+                        <Card className="flex justify-between items-center">
+                            <CardHeader className="bg-white">
+                                <CardTitle className="pt-3">Fullstack Developer Role</CardTitle>
+                                <CardDescription>Pending</CardDescription>
+                            </CardHeader>
+                            <CardContent className="bg-white p-6 space-y-3">
+                                <DialogTrigger asChild>
+                                    <Button>
+                                        View Details
+                                    </Button>
+                                </DialogTrigger>
+                            </CardContent>
+                        </Card>
+                    </div>
+                </CardContent>
+            </Card>
+            <ApplicationDetails/>
+        </Dialog>
+    );
+}
+export default Applications;
--- src/app/(main)/applicant/page.js
+++ src/app/(main)/applicant/page.js
@@ -4,9 +4,10 @@ import { Header } from "@/components/layout/header";
 import { AppSidebar } from "@/components/layout/sidebar";
 import { JobPosting } from "@/components/main/jobs/jobPosting";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
-import { hiringManager } from "@/constants";
+import { applicant } from "@/constants";
 import ApplicantProfile from "./(tabs)/profile/page";
 import { useState } from "react";
+import Applications from "./(tabs)/applications/page";
 
 const Index = () => {
     const [nav, setNav] = useState("Profile")
@@ -16,7 +17,7 @@ const Index = () => {
     }
     return (
         <SidebarProvider>
-            <AppSidebar screens={hiringManager} setNav={handleNav}/>
+            <AppSidebar screens={applicant} setNav={handleNav}/>
             <main className="w-full">
                 <div className="p-8 w-full space-y-5">
                     <Header>
@@ -24,7 +25,8 @@ const Index = () => {
                     </Header>
                     <div>
                         {nav === "JobListings" ? <JobPosting/> :
-                         nav === "Profile" ? <ApplicantProfile/> :null }
+                         nav === "Applications" ? <Applications/> :
+                         nav === "Profile" ? <ApplicantProfile/> : null }
                     </div>
                 </div>
             </main>
